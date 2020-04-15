bd6fffc8497eb3b38095abe8871a9a1a8f97ece6
Andrew Kowalczyk <andrewrkowalczyk@gmail.com>
2020-04-15 13:19:03 -0700

    add geo position hook and show the user their location

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import useGeoPosition from "../lib/useGeoPosition";
+
 export default function Home() {
-  return <div>Hello world!</div>;
+  const state = useGeoPosition();
+
+  if (state.error) {
+    return <div>{state.error.message}</div>;
+  }
+
+  return (
+    <div>
+      Hello! You are located at {state.latitude}, {state.longitude}
+    </div>
+  );
 }
